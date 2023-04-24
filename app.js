3a18714d107c377c638c5a7b78f2e5666de983bc
Ele <ejcatalan@hotmail.com>
2023-04-24 17:12:37 +0100

    logic for js added

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 let toc = document.querySelector("#table-of-contents");
-let headings = document.querySelector("h2");
+let headings = document.querySelectorAll("h2");
 
 let html = '';
 
+for (let heading of headings) {
+    html += `<li><a href="#${heading.id}">${heading.textContent}</a></li>`
+};
+
+if (html) {
+    toc.innerHTML = `
+    <p><strong>Table of Contents</strong></p>
+    <ol>
+    ${html}
+    </ol>
+    `;
+}
